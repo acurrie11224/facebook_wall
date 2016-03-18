b2ee7b8b247494c3b58600692cc66724ed6d7318
acurrie11224 <acurr7243@withs.org>
2016-03-18 14:00:02 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -1,6 +1,9 @@
 $(document).ready(function()
 {
 	$('#message').focus();
-
-	// YOUR CODE GOES HERE
+	$("#post-btn").click(function(){
+		var button = $("#message").val();
+	$('#wall').append("<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'><div>"+button+"</div>");
+	})
+$('#wall').float("left");
 });
